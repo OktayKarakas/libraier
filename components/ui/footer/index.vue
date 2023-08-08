b4bb654e1a77305b45d41d8494ad89df52fddcd3
Oktay <oktaykarakas71@gmail.com>
2023-08-08 16:59:08 +0300

    clear code added

--- components/ui/footer/index.vue
+++ components/ui/footer/index.vue
@@ -6,10 +6,10 @@
     />
     <div class="flex flex-col gap-5 text-white text-[12px] pl-10 pb-20">
       <div class="flex flex-col gap-2.5">
-        <h3 @click="changeOpenStatus('isProcutsOpen')">Products</h3>
+        <h3 @click="changeOpenStatus('isProductsOpen')">Products</h3>
         <div
           class="text-[10px] underline flex flex-col gap-2.5"
-          v-if="getRefValue('isProcutsOpen')"
+          v-if="reactiveVars.isProductsOpen.value"
         >
           <p class="text-[10px] underline">Prompts</p>
         </div>
@@ -18,7 +18,7 @@
         <h3 @click="changeOpenStatus('isAboutOpen')">About</h3>
         <div
           class="text-[10px] underline flex flex-col gap-2.5"
-          v-if="getRefValue('isAboutOpen')"
+          v-if="reactiveVars.isAboutOpen.value"
         >
           <p class="text-[10px] underline">Who we are</p>
           <p class="text-[10px] underline">Why libraier</p>
@@ -28,7 +28,7 @@
         <h3 @click="changeOpenStatus('isToolsOpen')">Tools</h3>
         <div
           class="text-[10px] underline flex flex-col gap-2.5"
-          v-if="getRefValue('isToolsOpen')"
+          v-if="reactiveVars.isToolsOpen.value"
         >
           <p class="text-[10px] underline">Categorize Prompts</p>
           <p class="text-[10px] underline">Write Prompts</p>
@@ -39,7 +39,7 @@
         <h3 @click="changeOpenStatus('isSupportOpen')">Support</h3>
         <div
           class="text-[10px] underline flex flex-col gap-2.5"
-          v-if="getRefValue('isSupportOpen')"
+          v-if="reactiveVars.isSupportOpen.value"
         >
           <p>Help Section</p>
           <p>libraier@gmail.com</p>
@@ -50,23 +50,18 @@
 </template>
 
 <script setup>
-const reactiveVars = new Map([
-  ["isProcutsOpen", ref(false)],
-  ["isAboutOpen", ref(false)],
-  ["isToolsOpen", ref(false)],
-  ["isSupportOpen", ref(false)],
-]);
+const reactiveVars = {
+  isProductsOpen: ref(false),
+  isAboutOpen: ref(false),
+  isToolsOpen: ref(false),
+  isSupportOpen: ref(false),
+};
 
 function changeOpenStatus(refName) {
-  const refVariable = reactiveVars.get(refName);
-  if (refVariable !== undefined && refVariable) {
+  const refVariable = reactiveVars[refName];
+  if (refVariable !== undefined) {
     refVariable.value = !refVariable.value;
   }
-}
-
-function getRefValue(refName) {
-  const refVariable = reactiveVars.get(refName);
-  return refVariable !== undefined ? refVariable.value : false;
 }
 </script>
 
