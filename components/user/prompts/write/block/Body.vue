87bb4926d4fa60ef0af76019377a95d352cf1b8b
Oktay <oktaykarakas71@gmail.com>
2023-08-17 03:31:34 +0300

    image add part added to write prompts & category logic added.

--- components/user/prompts/write/block/Body.vue
+++ components/user/prompts/write/block/Body.vue
@@ -11,26 +11,72 @@
           <input
             id="titleInput"
             name="titleInput"
-            class="bg-[#252525] border borderColorShadow w-[253px] h-[28px] text-white rounded-[5px]"
+            class="bg-[#252525] border borderColor w-[253px] h-[28px] text-white rounded-[5px]"
           />
         </div>
         <div class="flex flex-col items-center gap-[20px] pt-[40px]">
           <label
-            for="titleInput"
+            for="category"
             class="text-white font-semibold text-[20px] leading-[20px] w-[253px]"
             >Category</label
           >
-          <div class="flex">
-            <input
-              id="titleInput"
-              name="titleInput"
-              class="bg-[#252525] border borderColorShadow w-[226px] h-[28px] text-white rounded-[5px] rounded-tr-[0px] rounded-br-[0px]"
-            />
-            <div
-              class="w-[27px] h-[28px] border borderColor flex items-center justify-center rounded-tr-[5px] rounded-br-[5px] bg-[#252525]"
-            >
-              <img src="~/assets/user/icons/ChevronDown.svg" />
+          <div>
+            <div class="flex">
+              <input
+                id="titleInput"
+                name="category"
+                class="bg-[#252525] border borderColor w-[226px] h-[28px] text-white rounded-[5px] rounded-tr-[0px] rounded-br-[0px]"
+                @input="changeCategoryName"
+              />
+              <div
+                class="w-[27px] h-[28px] border borderColor flex items-center justify-center rounded-tr-[5px] rounded-br-[5px] bg-[#252525]"
+              >
+                <img src="~/assets/user/icons/ChevronDown.svg" />
+              </div>
             </div>
+            <div class="w-[251px] mx-auto bg-[#252525]" v-if="categoryName">
+              <div
+                class="pl-[20px] py-[10px] border-b border-black"
+                @click="createCategory"
+              >
+                <p class="text-white text-[8px] font-semibold">
+                  Create "{{ categoryName }}" Category
+                </p>
+              </div>
+              <div class="pl-[20px] py-[10px] border-b border-black">
+                <p class="text-white text-[8px] font-semibold">
+                  Smart Brain Solves
+                </p>
+              </div>
+            </div>
+            <div class="flex flex-col items-center gap-[20px] pt-[40px]">
+              <p
+                class="text-white font-semibold text-[20px] leading-[20px] w-[251px]"
+              >
+                Thumbnail Image
+              </p>
+              <label
+                for="ThumbnailImage"
+                class="w-[251px] h-[81px] bg-[#252525] border borderColor rounded-[5px]"
+                @dragover.prevent="onDragOver"
+                @drop.prevent="onDrop"
+              >
+              </label>
+              <input
+                ref="fileInput"
+                id="ThumbnailImage"
+                name="ThumbnailImage"
+                type="file"
+                accept="image/*"
+                @change="handleFileInput"
+                style="display: none"
+              />
+            </div>
+            <img
+              v-if="sanitizedImageUrl"
+              :src="sanitizedImageUrl"
+              alt="Uploaded Image"
+            />
           </div>
         </div>
       </form>
@@ -38,13 +84,62 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DOMPurify from "dompurify";
+
+const categoryName = ref(false);
+
+const changeCategoryName = (event) => {
+  const element = event.target.value.trim();
+  if (element !== "") {
+    categoryName.value = element;
+  } else {
+    categoryName.value = false;
+  }
+};
+
+const createCategory = () => {
+  console.log(categoryName.value);
+};
+
+const imageUrl = ref("");
+const sanitizedImageUrl = ref("");
+
+const onDragOver = (event) => {
+  event.preventDefault();
+};
+
+const onDrop = (event) => {
+  event.preventDefault();
+  const files = event.dataTransfer.files;
+  handleImage(files[0]);
+};
+
+const handleFileInput = (event) => {
+  const file = event.target.files[0];
+  handleImage(file);
+};
+
+const handleImage = (file) => {
+  if (!file) return;
+
+  if (!file.type.startsWith("image/")) {
+    console.error("Invalid file type. Please upload an image.");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    imageUrl.value = event.target.result;
+
+    const sanitizedUrl = DOMPurify.sanitize(imageUrl.value);
+    sanitizedImageUrl.value = sanitizedUrl;
+  };
+  reader.readAsDataURL(file);
+};
+</script>
 
 <style scoped>
-.borderColorShadow {
-  border: 1px solid rgba(255, 255, 255, 0.35);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
 .borderColor {
   border: 1px solid rgba(255, 255, 255, 0.35);
 }
